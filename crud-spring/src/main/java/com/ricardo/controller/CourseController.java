8e3967e39f5e0166f595461610c2db3f839c73e2
Ricardo Alves <dev.rical21@gmail.com>
2023-07-01 22:09:29 -0300

    Criando a camada de serviços

--- crud-spring/src/main/java/com/ricardo/controller/CourseController.java
+++ crud-spring/src/main/java/com/ricardo/controller/CourseController.java
@@ -2,11 +2,10 @@ package com.ricardo.controller;
 
 
 import com.ricardo.model.Course;
-import com.ricardo.repository.CourseRepository;
+import com.ricardo.service.CourseService;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
-import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
@@ -17,57 +16,51 @@ import java.util.List;
 @Validated
 @RestController //Esta anotação aviso ao spring que esta classe possui um endpoint (url que acessa uma API)
 @RequestMapping("/api/courses") //Este é o endpoint que estará exposto na API
-@AllArgsConstructor //Esta anotação informa ao spring que a injeção de dependências está sendo feita via construtor, isso é boa prática. Isso dispensa a anotação '@Autowired'
 public class CourseController {
 
-    //Este atributo está possibilitando que o spring faça a injeção de dependência deste repositório nesta Controller
-    //O termo 'final' é para garantir que esta instância será utilizada sem nenhuma modificação em seus dados
-    private final CourseRepository courseRepository;
+    //Estes atributos está possibilitando que o spring faça a injeção de dependências destas classes nesta Controller.
+    //O termo 'final' é para garantir que esta instância será utilizada sem nenhuma modificação de dados.
+    private final CourseService courseService;
 
-    //Anotação para o método GET
-    @GetMapping
-    public List<Course> list() {
-        return courseRepository.findAll();
+    public CourseController(CourseService courseService) {
+        this.courseService = courseService;
     }
 
-    //Anotação para o método POST
-    @PostMapping
-    @ResponseStatus(code = HttpStatus.CREATED)
-    public Course create(@RequestBody @Valid Course course) {
-        return courseRepository.save(course);
+    @GetMapping
+    public List<Course> list() {
+        return courseService.list();
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<Course> buscarCurso(@PathVariable @NotNull @Positive Long id) {
-        return courseRepository.findById(id)
+        return courseService.buscarCurso(id)
                 .map(registroEncontrado -> ResponseEntity.ok().body(registroEncontrado))
                 .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    @ResponseStatus(code = HttpStatus.CREATED)
+    public Course create(@RequestBody @Valid Course course) {
+        return courseService.create(course);
     }
 
     @PutMapping("/{id}")
     public ResponseEntity<Course> atualizaCurso(@Valid @PathVariable @NotNull @Positive Long id,
                                                 @RequestBody @Valid Course course) {
 
-        return courseRepository.findById(id)
-                .map(registroEncontrado -> {
-                    registroEncontrado.setName(course.getName());
-                    registroEncontrado.setCategory(course.getCategory());
-                    Course update = courseRepository.save(registroEncontrado);
-                    return ResponseEntity.ok().body(update);
-                })
+        return courseService.atualizaCurso(id, course)
+                .map(registroEncontrado -> ResponseEntity.ok().body(registroEncontrado))
                 .orElse(ResponseEntity.notFound().build());
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> removeCurso(@PathVariable @NotNull @Positive Long id) {
 
-        if(!courseRepository.existsById(id)) {
+        if(courseService.removeCurso(id)) {
+            return ResponseEntity.noContent().build();
+        } else {
             return ResponseEntity.notFound().build();
         }
-
-        courseRepository.deleteById(id);
-
-        return ResponseEntity.noContent().build();
 
     }
 
